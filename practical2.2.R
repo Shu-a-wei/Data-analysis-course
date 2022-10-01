e186ec55cb846acec9236149e283181c4720840e
Shu-a-wei <113423921+Shu-a-wei@users.noreply.github.com>
2022-10-01 11:39:56 +0800

    Practical 2.2

--- practical2.2.R
+++ practical2.2.R
@@ -13,3 +13,11 @@ df[df=="nutrient"] <-"enriched"
 df[9][df[9]=="nutrient"] <-"enriched"
 rairuoho[9][rairuoho[9]=="nutrient"] <-"enriched"
 Rairuho_wide <- rairuoho %>% pivot_wider(day3:day8, names_to = "period", values_to = "length")
+Rairuho_wide <- rairuoho%>% pivot_wider(names_from="day3", values_from="dayRairuho_wide <- pivot_wider(day3:day8, names_to =period, values_to= length) 8")
+Rairuho_wide<- rairuoho %>% pivot_wider(day3:day8, names_from="day3:day8", values_from="lenght")
+Rairuoho_long <- rairuoho%>% pivot_longer(day3:day8, names_to = "day", values_to = "lenght")
+Rairuoho_long
+unite("spatial", spatial1:spatial2, "_", remove=TRUE)
+?unite()
+rairuoho_long_spatial<- Rairuoho_long %>% unite("spatial", spatial1:spatial2, sep="_", remove=TRUE, na.rm=FALSE)
+rairuoho_removed<- subset(rairuoho_long_spatial, select=-c(row,column))
