25abbccf020bc6a588bc4eb7d0e0b4d6f7ab259f
Shu-a-wei <113423921+Shu-a-wei@users.noreply.github.com>
2022-10-04 17:23:30 +0800

    Practice 3.2

---
+++ Practice 3.2.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+#creating the dataframe from the table in R, calling it Bubble
+Person <- c("subject_1", "subject_2", "subject_3", "subject_4", "subject_5")
+Before_diet <- c(104, 95, 87, 77, 112)
+After_diet <-c(96, 91, 81, 75, 118)
+Bubble  <- data.frame(Person, Before_diet, After_diet)
+str(Bubble)
+#creating the components of WEIGHT_LOSS
+Weightloss_percentage <- c((Bubble$Before_diet-Bubble$After_diet)/Bubble$Before_diet*100)
+Subject <- Person
+Weight_loss_table <- data.frame (Subject,Weightloss_percentage)
