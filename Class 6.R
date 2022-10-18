ab01aafc9fbf178f23e37e13f2beebd4a4353507
Shu-a-wei <113423921+Shu-a-wei@users.noreply.github.com>
2022-10-18 16:57:54 +0800

    Class 6

--- Class 6.R
+++ Class 6.R
@@ -9,7 +9,7 @@ plot(Petal.Length ~ Petal.Width, dat = iris,
      col = scales::alpha(col.iris, 0.2))
 
 legend(x="bottomright", pch= 19, cex=1.0, legend= c("versicolor","setosa", "virginica"), col=levels(as.factor(scales::alpha(col.iris, 0.2))))
-library(plot.new)
+# library(plot.new)
 #did not work
 library(maptools)
 library(rgdal)
@@ -25,3 +25,13 @@ library(mapr)
 library(marmap)
 library(leaflet)
 #did not want to do it?
+install.packages("leaflet")
+install.packages("maptools")
+data("wrld_simpl")
+library("maptools")
+data("wrld_simpl")
+plot(wrld_simpl)
+plot(wrld_simpl,xlim=c(100,130),ylim=c(-20,50),col='olivedrab3',bg='lightblue')
+plot(wrld_simpl,xlim=c(120,125),ylim=c(20,30),col='olivedrab3',bg='lightblue')
+#resolution is too low
+install.packages("readOGR")
